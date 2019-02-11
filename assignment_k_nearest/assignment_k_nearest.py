95ee62bdeb756ee1ffe0777910dfb1da27ae3d68
Kevin Nijmeijer <kevin.nijmeijer@student.hu.nl>
2019-02-11 15:30:52 +0100

    Added kNN functionality. Todo: Code commenting and k-Means implementation.

--- assignment_k_nearest/assignment_k_nearest.py
+++ assignment_k_nearest/assignment_k_nearest.py
@@ -1,26 +1,28 @@
 import numpy as np
 import random
+import time
 
 # Calculate distance K-means
 
-def ParseValuesFromDataset():
-    data = np.genfromtxt("assignment_k_nearest\\dataset.csv", delimiter=";", usecols=[1,2,3,4,5,6,7], converters={5: lambda s: 0 if s == b"-1" else float(s), 7: lambda s: 0 if s == b"-1" else float(s)})
+def ParseValuesFromDataset(filename):
+    data = np.genfromtxt(filename, delimiter=";", usecols=[1,2,3,4,5,6,7], converters={5: lambda s: 0 if s == b"-1" else float(s), 7: lambda s: 0 if s == b"-1" else float(s)})
     return data
 
-def ParseDatesFromDataset():
-    dates = np.genfromtxt("assignment_k_nearest\\dataset.csv", delimiter=";", usecols=[0])
+def ParseDatesFromDataset(filename):
+    dates = np.genfromtxt(filename, delimiter=";", usecols=[0])
     return dates
 
 def FormLabels(dates):
     labels = []
     for label in dates:
-        if label < 20000301:
+        dateWithoutYear = label%10000
+        if dateWithoutYear < 301:
             labels.append("winter")
-        elif 20000301 <= label < 20000601:
+        elif 301 <= dateWithoutYear < 601:
             labels.append("lente") 
-        elif 20000601 <= label < 20000901:
+        elif 601 <= dateWithoutYear < 901:
             labels.append("zomer") 
-        elif 20000901 <= label < 20001201: 
+        elif 901 <= dateWithoutYear < 1201: 
             labels.append("herfst")
         else: # from 01-12 to end of year 
             labels.append("winter")
@@ -32,33 +34,74 @@ def CalculateEuclideanDistance(pointA, pointB):
     sum = 0
     for i in range(0, len(pointA)):
         sum += (pointA[i] - pointB[i])**2
-
-        # print(sum)
-
     sum = sum/len(pointA)
     return sum
-    # print(sum)
 
-def GenerateCentroids(data, k):
-    if len(data) <= k:
-        return
-    centroids = [None]*k
-    for n in range(k):
-        centroids[n]  = data[random.randint(0, len(data))]
-    return centroids
+def DetermineClosestIndices(k, distances):
+    closestIndices = []
+    currentIndex = 0
+    while currentIndex < k:
+        indexOfSmallest = distances.index(min(distances))
+        closestIndices.append(indexOfSmallest)
+        del(distances[indexOfSmallest])
+        currentIndex += 1
+    return closestIndices
+
+def predictLabel(labels):    
+    predictedLabel = max(set(labels), key=labels.count)
+
+    winters = labels.count("winter")
+    lentes = labels.count("lente")
+    zomers = labels.count("zomer")
+    herfsts = labels.count("herfst")
+
+    # print("Winters:",winters,", Lentes:",lentes, ", Zomers:",zomers, ", Herfsts:", herfsts)
+    
+    return predictedLabel
 
 def main():
-    data = ParseValuesFromDataset()
-    dates = ParseDatesFromDataset()
-    labels = FormLabels(dates)
+    originalData = ParseValuesFromDataset("assignment_k_nearest\\dataset.csv")
+    originalDates = ParseDatesFromDataset("assignment_k_nearest\\dataset.csv")
+    originalLabels = FormLabels(originalDates)
 
-    k = 2
+    newData = ParseValuesFromDataset("assignment_k_nearest\\validation1.csv")
+    newDates = ParseDatesFromDataset("assignment_k_nearest\\validation1.csv")
+    newLabels = FormLabels(newDates)
 
-    centroids = GenerateCentroids(data, k)
+    # ksForDict = []
+    # correctForDict = []
+
+    k = 1
+    while k <= 100:
+
+        # ksForDict.append(k)
+        currentPointIndex = 0
+        correct = 0
+        for newPoint in newData:
+            labels = []
+            distances = []
+            for oldPoint in originalData:
+                distances.append(CalculateEuclideanDistance(newPoint, oldPoint))
+            closestIndices = DetermineClosestIndices(k, distances)
+            for index in closestIndices:
+                labels.append(originalLabels[index])
+            predictedLabel = predictLabel(labels)
+            realLabel = newLabels[currentPointIndex]
+            
+
+            if predictedLabel == realLabel:
+                correct += 1
+            # else:
+            #     print("Predicted:",predictedLabel,"||| Real:",realLabel)
+
+            currentPointIndex += 1
+        # correctForDict.append(correct)
+        print("K =", k, "- correct answers:", correct)
+        k += 1
+
     
-    print(centroids)
-
-
+    # print(dict(zip(ksForDict,correctForDict)))
+    
 
 # Parsing function
 if __name__ == "__main__":
