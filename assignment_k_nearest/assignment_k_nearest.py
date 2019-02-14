7b4d305cd27d6221f863539ae0d35d64ef75c282
Wiebe van Breukelen <wiebe.vanbreukelen@student.hu.nl>
2019-02-14 09:42:01 +0100

    Cleaned up assigment_k_nearest.py

--- assignment_k_nearest/assignment_k_nearest.py
+++ assignment_k_nearest/assignment_k_nearest.py
@@ -4,49 +4,71 @@ import math
 from collections import Counter
 from operator import itemgetter
 
-# Calculate distance K-nearest, by Wiebe van Breukelen and Kevin Nijmeijer.
+# Calculate distance K-nearest, by Wiebe van Breukelen.
 
 
-def ParseDataset(file, isValidationSet):
+def ParseDataset(file):
+    """ Parse all data points within a given .csv dataset.
+
+    Arguments:
+        file {string} -- File path.
+
+    Returns:
+        nparray -- Numpy array containing all data points.
+        nparray -- Numpy array containing all labels.
+    """
+
     data = np.genfromtxt(file, delimiter=";", usecols=[1, 2, 3, 4, 5, 6, 7], converters={
-                         5: lambda s: 0 if s == b"-1" else float(s), 7: lambda s: 0 if s == b"-1" else float(s)})
-    dates = np.genfromtxt(
+        5: lambda s: 0 if s == b"-1" else float(s),
+        7: lambda s: 0 if s == b"-1" else float(s)
+    })
+
+    labels = np.genfromtxt(
         file, delimiter=";", usecols=[0])
 
-    labels = []
+    return data, labels
 
-    if isValidationSet:
-        for label in dates:
-            if label < 20010301:
-                labels.append("winter")
-            elif 20010301 <= label < 20010601:
-                labels.append("lente")
-            elif 20010601 <= label < 20010901:
-                labels.append("zomer")
-            elif 20010901 <= label < 20011201:
-                labels.append("herfst")
-            else:  # from 01-12 to end of year
-                labels.append("winter")
-    else:
-        for label in dates:
-            if label < 20000301:
-                labels.append("winter")
-            elif 20000301 <= label < 20000601:
-                labels.append("lente")
-            elif 20000601 <= label < 20000901:
-                labels.append("zomer")
-            elif 20000901 <= label < 20001201:
-                labels.append("herfst")
-            else:  # from 01-12 to end of year
-                labels.append("winter")
 
-    return {'data': data, 'labels': labels}
+def DateToSeason(date):
+    """ Convert any given date to a season (in Dutch).
+
+    Arguments:
+        date {int} -- Date.
+
+    Returns:
+        string -- Season in Dutch.
+    """
+
+    dateWithoutYear = date % 10000
+
+    if dateWithoutYear < 301:
+        return "winter"
+    elif 301 <= dateWithoutYear < 601:
+        return "lente"
+    elif 601 <= dateWithoutYear < 901:
+        return "zomer"
+    elif 901 <= dateWithoutYear < 1201:
+        return "herfst"
+    else:  # from 01-12 to end of year
+        return "winter"
 
 
 def CalculateEuclideanDistance(pointA, pointB):
+    """ Calculate the enclidean distance between two multidimensional points.
+
+    Note that the amount of dimensions of both points are required to be the same.
+
+    Arguments:
+        pointA {nparray} -- First point.
+        pointB {nparray} -- Second point.
+
+    Returns:
+        float -- The calculated euclidean distance.
+    """
+
     # Check if we are dealing with the same number of dimensions.
     if len(pointA) != len(pointB):
-        exit()
+        return None
     sum = 0
     for i in range(len(pointA)):
         sum += math.pow(pointA[i] - pointB[i], 2)
@@ -54,76 +76,80 @@ def CalculateEuclideanDistance(pointA, pointB):
     return math.sqrt(sum)
 
 
-def GetNeighbours(trainingSet, pointSearch, k):
+def GetNeighbours(dataset, labels, referencePoint, k):
+    """ Receive the k closest points to a given reference point by calculating the using the enclidean distance.
+
+    Arguments:
+        dataset {nparray} -- Numpy array containing the position points.
+        labels {nparray} -- Numpy array containing all the labels of the points within the dataset.
+        referencePoint {list} -- Reference point.
+        k {int} -- Amount of points to return closest to the reference point.
+
+    Returns:
+        list -- K closest points to the reference point.
+    """
+
     distances = []
 
-    for index in range(len(trainingSet['data'])):
+    for index in range(len(dataset)):
         distances.append(
-            [index, trainingSet['labels'][index], CalculateEuclideanDistance(
-                pointSearch, trainingSet['data'][index])])
+            [CalculateEuclideanDistance(
+                referencePoint, dataset[index]), DateToSeason(labels[index])])
 
     # Sort the points by their distances.
-    distances.sort(key=itemgetter(2))
+    distances.sort(key=itemgetter(0))
 
     return distances[:k]
 
 
-def GetMostCommon(labels):
+def MostCommonInList(list):
+    """ Return the most common value within a list. Behaviour is undefined when using multitype lists.
+
+    Arguments:
+        list {list} -- List containing values.
+
+    Returns:
+        [mizxed] -- Most common value within the list.
+    """
+
     # Select the most common season within all classified points.
-    return Counter(labels).most_common()[0][0]
+    return Counter(list).most_common()[0][0]
 
 
 def main():
     # Parse both datasets.
-    dataset = ParseDataset("assignment_k_nearest\\dataset.csv", False)
-    trainingSet = ParseDataset("assignment_k_nearest\\validation1.csv", True)
+    dataset, datasetLabels = ParseDataset(
+        "assignment_k_nearest\\validation1.csv")
+    trainingSet, trainingSetLabels = ParseDataset(
+        "assignment_k_nearest\\dataset.csv")
 
-    # Check every k from 2 to 63
-    for k in range(59, 75):
+    maxK = 63
+
+    # Check every k from 2 to maxK
+    for k in range(2, maxK):
         # Iterate through the whole dataset.
 
         totalSuccess = 0
-        totalFail = 0
 
-        for index in range(len(dataset['data'])):
-            neighbours = GetNeighbours(trainingSet, dataset['data'][index], k)
+        for index in range(len(dataset)):
+            # print(entry)
+            neighbours = GetNeighbours(
+                trainingSet, trainingSetLabels, dataset[index], k)
+
+            # print(neighbours)
 
             labels = [i[1] for i in neighbours]
 
-            result = GetMostCommon(labels)
-
-            # print("Expected: {} | Actual: {}".format(
-            #   dataset['labels'][index], result))
-
-            if result is dataset['labels'][index]:
-                # We guessed it right, the season is correct.
+            result = MostCommonInList(labels)
+            # result = get_most_common_label(neighbours)
+            if result == DateToSeason(datasetLabels[index]):
                 totalSuccess += 1
-            else:
-                # We failed.
-                totalFail += 1
 
         print("=============== K: {} ===============\n".format(k))
-
-        print("Total success: {} | Total fail: {}".format(totalSuccess, totalFail))
-
-        print(len(trainingSet['labels']))
-        print(len(trainingSet['data']))
-
-        print("Error percentage: {}%".format(
-            (100 * totalFail) / (totalSuccess + totalFail)))
-
         print("Success percentage: {}%\n".format(
-            (100 * totalSuccess) / (totalSuccess + totalFail)))
+            (100 * totalSuccess) / len(dataset)))
 
 
 # Invoke the main function.
 if __name__ == "__main__":
     main()
-
-# def GenerateCentroids(data, k):
-#     if len(data) <= k:
-#         return
-#     centroids = []
-#     for n in range(k):
-#         centroids.append(data[random.randint(0, len(data))])
-#     return centroids
