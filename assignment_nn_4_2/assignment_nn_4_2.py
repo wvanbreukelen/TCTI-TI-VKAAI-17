f3dc73647e9b68c1e74f62df39db5bca89593ba5
Wiebe van Breukelen <wiebe.vanbreukelen@student.hu.nl>
2019-02-26 13:31:33 +0100

    Work in progress...

--- assignment_nn_4_2/assignment_nn_4_2.py
+++ assignment_nn_4_2/assignment_nn_4_2.py
@@ -1,50 +1,112 @@
-class Bias:
-    self __init__(self, value, weight):
-        pass
+import math
+import random
+
+
+# class Bias:
+#     self __init__(self, value, weight):
+#         self.weight = weight
+#         self.value = value
 
 
 class Neuron:
-    def __init__(self, bias, weights=[]):
+    def __init__(self, bias, weights=[], defaultOutput=None):
         self.bias = bias
         self.weights = weights
-        self.outputValue = None
+        self.inputs = []
+        self.output = defaultOutput
 
-    def CalculateOutput(self, input):
-        sum = 0.0
+    def SetInputWeights(self, weights):
+        self.weights = weights
 
-        for weight in weights:
-            sum += input * weight
+    def AddInputWeight(self, weight):
+        print("Adding weight {} to neuron...".format(weight))
+        self.weights.append(weight)
 
-        sum += bias
+    def CalculateOutput(self, inputs):
+        self.inputs = inputs
+        self.output = 0.0
 
-        # Sum is a value not between 0.0 and 1.0
+        for index in range(len(self.inputs)):
+            self.output += self.inputs[index] * self.weights[index]
 
-        return sum
+        self.output = self.PerformSigmoid(self.output)
+
+        return self.output
+
+    def PerformSigmoid(self, value):
+        return 1 - math.tanh(math.tanh(value))
+
+    def IsInputNeuron(self):
+        return (len(self.weights) is 0)
 
 
 class NeuronLayer:
-    def __init__(self, neuronAmount, bias):
-        self.neurons = [Neuron(bias)
-                        for i in range(neuronAmount)]
+    def __init__(self, neuronAmount, bias, defaultOutputs=[]):
+        if len(defaultOutputs):
+            self.neurons = [Neuron(bias, defaultOutputs[i])
+                            for i in range(neuronAmount)]
+        else:
+            self.neurons = [Neuron(bias)
+                            for i in range(neuronAmount)]
 
-        for i in range(neuronAmount):
+    def IsInputLayer(self):
+        for neuron in self.neurons:
+            if not neuron.IsInputNeuron():
+                return False
+
+        return True
 
 
 class NeuralNetwork:
-    def __init__(self, inputs, neuronsInHiddenLayer, neuronsInOutputLayer, bias):
-        self.inputs = inputs
+    def __init__(self, inputs, neuronsInHiddenLayer, neuronsInOutputLayer, bias, hiddenLayerWeights=[], outputLayerWeights=[]):
+        #self.inputs = inputs
+        self.inputLayer = NeuronLayer(len(inputs[0]), bias, inputs)
         self.hiddenLayer = NeuronLayer(neuronsInHiddenLayer, bias)
         self.outputLayer = NeuronLayer(neuronsInOutputLayer, bias)
 
-    def initializeHiddenWeights():
-        # Initialize the we-> hidden layer neurons.
+        self.initializeHiddenLayerWeights(hiddenLayerWeights)
+        self.initializeOutputLayerWeights(outputLayerWeights)
+
+    def FeedForward(self):
         pass
+
+    def initializeHiddenLayerWeights(self, hiddenLayerWeights=[]):
+        weightIndex = 0
+
+        # Initialize the we-> hidden layer neurons.
+        for hiddenLayerNeuron in self.hiddenLayer.neurons:
+            for inputLayerNeuron in self.inputLayer.neurons:
+                if len(hiddenLayerWeights):
+                    hiddenLayerNeuron.AddInputWeight(
+                        hiddenLayerWeights[weightIndex])
+                else:
+                    hiddenLayerNeuron.AddInputWeight(random.uniform(0, 1))
+
+                weightIndex += 1
+
+    def initializeOutputLayerWeights(self, outputLayerWeights=[]):
+        weightIndex = 0
+
+        # Initialize the we-> hidden layer neurons.
+        for outputLayerNeuron in self.outputLayer.neurons:
+            for hiddenLayerNeuron in self.hiddenLayer.neurons:
+                if len(outputLayerWeights):
+                    outputLayerNeuron.AddInputWeight(
+                        outputLayerWeights[weightIndex])
+                else:
+                    outputLayerNeuron.AddInputWeight(random.uniform(0, 1))
+
+                weightIndex += 1
 
 
 def main():
     print("Hello World!")
 
-    nn = NeuralNetwork([[0, 0], [0, 1], [1, 0], [1, 1]], 2, 1, -1)
+    nn = NeuralNetwork([[0, 0], [0, 1], [1, 0], [1, 1]],
+                       2, 1, -1, [2.0, 3.0, 4.0, 1.0], [0.9, 0.2])
+
+    # nn = NeuralNetwork([[0, 0], [0, 1], [1, 0], [1, 1]],
+    #                   2, 1, -1)
 
 
 # Invoke the main function.
