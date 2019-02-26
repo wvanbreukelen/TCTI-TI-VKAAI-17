c35bb9ed89e1d1df8ecc8ceaa505cf8e3218354d
Wiebe van Breukelen <wiebe.vanbreukelen@student.hu.nl>
2019-02-26 14:58:51 +0100

    Finished feed forward implementation.

--- assignment_nn_4_2/assignment_nn_4_2.py
+++ assignment_nn_4_2/assignment_nn_4_2.py
@@ -2,17 +2,11 @@ import math
 import random
 
 
-# class Bias:
-#     self __init__(self, value, weight):
-#         self.weight = weight
-#         self.value = value
-
-
 class Neuron:
-    def __init__(self, bias, weights=[], defaultOutput=None):
-        self.bias = bias
-        self.weights = weights
+    def __init__(self, defaultOutput=None):
+        self.weights = []
         self.inputs = []
+        self.isInputNeuron = (defaultOutput is not None)
         self.output = defaultOutput
 
     def SetInputWeights(self, weights):
@@ -23,87 +17,128 @@ class Neuron:
         self.weights.append(weight)
 
     def CalculateOutput(self, inputs):
+        if self.isInputNeuron:
+            return self.output
+
         self.inputs = inputs
         self.output = 0.0
 
         for index in range(len(self.inputs)):
             self.output += self.inputs[index] * self.weights[index]
 
-        self.output = self.PerformSigmoid(self.output)
+        self.output = math.tanh(self.output)
 
-        return self.output
-
-    def PerformSigmoid(self, value):
-        return 1 - math.tanh(math.tanh(value))
-
-    def IsInputNeuron(self):
-        return (len(self.weights) is 0)
+    def __str__(self):
+        return "Weights: {}\n Inputs: {}\nOutputs: {}\nIs preset: {}\n".format(
+            self.weights, self.inputs, self.output, self.isInputNeuron)
 
 
 class NeuronLayer:
-    def __init__(self, neuronAmount, bias, defaultOutputs=[]):
+    def __init__(self, neuronAmount, defaultOutputs=[]):
         if len(defaultOutputs):
-            self.neurons = [Neuron(bias, defaultOutputs[i])
+            self.neurons = [Neuron(defaultOutputs[i])
                             for i in range(neuronAmount)]
         else:
-            self.neurons = [Neuron(bias)
+            self.neurons = [Neuron()
                             for i in range(neuronAmount)]
+
+    def GetOutput(self):
+        result = []
+
+        for neuron in self.neurons:
+            result.append(neuron.output)
+
+        return result
+
+    def SetOutput(self, inputs):
+        for neuron in self.neurons:
+            neuron.CalculateOutput(inputs)
 
     def IsInputLayer(self):
         for neuron in self.neurons:
-            if not neuron.IsInputNeuron():
+            if not neuron.isInputNeuron:
                 return False
 
         return True
 
+    def __repr__(self):
+        print("Is input layer: {}\n".format(self.IsInputLayer()))
+
+        for neuron in self.neurons:
+            print(neuron)
+
+        return str()
+
 
 class NeuralNetwork:
     def __init__(self, inputs, neuronsInHiddenLayer, neuronsInOutputLayer, bias, hiddenLayerWeights=[], outputLayerWeights=[]):
-        #self.inputs = inputs
-        self.inputLayer = NeuronLayer(len(inputs[0]), bias, inputs)
-        self.hiddenLayer = NeuronLayer(neuronsInHiddenLayer, bias)
-        self.outputLayer = NeuronLayer(neuronsInOutputLayer, bias)
+        # self.inputs = inputs
+        self.inputLayer = NeuronLayer(len(inputs[0]), inputs[0])
 
-        self.initializeHiddenLayerWeights(hiddenLayerWeights)
-        self.initializeOutputLayerWeights(outputLayerWeights)
+        # Add the bias neuron
+        self.inputLayer.neurons.append(Neuron(bias))
+
+        self.hiddenLayer = NeuronLayer(neuronsInHiddenLayer)
+
+        # Add the bias neuron
+        self.hiddenLayer.neurons.append(Neuron(bias))
+
+        self.outputLayer = NeuronLayer(neuronsInOutputLayer)
+
+        self.InitializeWeightsBetweenLayers(
+            self.inputLayer, self.hiddenLayer, hiddenLayerWeights)
+        self.InitializeWeightsBetweenLayers(
+            self.hiddenLayer, self.outputLayer, outputLayerWeights)
 
     def FeedForward(self):
+        self.hiddenLayer.SetOutput(self.inputLayer.GetOutput())
+        self.outputLayer.SetOutput(self.hiddenLayer.GetOutput())
+
+    def Backpropagate(self):
         pass
 
-    def initializeHiddenLayerWeights(self, hiddenLayerWeights=[]):
+    def InitializeWeightsBetweenLayers(self, layerOne, layerTwo, hiddenLayerWeights=[]):
         weightIndex = 0
-
         # Initialize the we-> hidden layer neurons.
-        for hiddenLayerNeuron in self.hiddenLayer.neurons:
-            for inputLayerNeuron in self.inputLayer.neurons:
-                if len(hiddenLayerWeights):
-                    hiddenLayerNeuron.AddInputWeight(
-                        hiddenLayerWeights[weightIndex])
-                else:
-                    hiddenLayerNeuron.AddInputWeight(random.uniform(0, 1))
+        for layerTwoNeuron in layerTwo.neurons:
+            if not layerTwoNeuron.isInputNeuron:
+                for layerOneNeuron in layerOne.neurons:
+                    # Skip neurons with predetermined output. @wvanbreukelen fix naming convention.
+                    if len(hiddenLayerWeights) > weightIndex:
+                        layerTwoNeuron.AddInputWeight(
+                            hiddenLayerWeights[weightIndex])
+                    else:
+                        layerTwoNeuron.AddInputWeight(random.uniform(0, 1))
 
-                weightIndex += 1
+                    weightIndex += 1
 
-    def initializeOutputLayerWeights(self, outputLayerWeights=[]):
-        weightIndex = 0
+    def __repr__(self):
+        print("INPUT LAYER")
+        print(self.inputLayer)
 
-        # Initialize the we-> hidden layer neurons.
-        for outputLayerNeuron in self.outputLayer.neurons:
-            for hiddenLayerNeuron in self.hiddenLayer.neurons:
-                if len(outputLayerWeights):
-                    outputLayerNeuron.AddInputWeight(
-                        outputLayerWeights[weightIndex])
-                else:
-                    outputLayerNeuron.AddInputWeight(random.uniform(0, 1))
+        print("HIDDEN LAYER")
+        print(self.hiddenLayer)
 
-                weightIndex += 1
+        print("OUTPUT LAYER")
+        print(self.outputLayer)
+
+        return str()
 
 
 def main():
     print("Hello World!")
 
-    nn = NeuralNetwork([[0, 0], [0, 1], [1, 0], [1, 1]],
-                       2, 1, -1, [2.0, 3.0, 4.0, 1.0], [0.9, 0.2])
+    nn = NeuralNetwork([[1, 1], [0, 1], [1, 0], [1, 1]],
+                       2, 1, -1)
+
+    # nn = NeuralNetwork([[0, 1], [0, 1], [1, 0], [1, 1]],
+    #                    2, 1, 1, [1.0, 1.0, 0.0, -1.0, -1.0, 0.0], [1.0, 1.0, 0.0])
+
+    print(nn)
+
+    nn.FeedForward()
+
+    print(nn)
 
     # nn = NeuralNetwork([[0, 0], [0, 1], [1, 0], [1, 1]],
     #                   2, 1, -1)
